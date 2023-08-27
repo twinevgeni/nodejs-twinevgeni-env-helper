e77463e1c76627296746858e5d22f5c3b590c88c
Ижболдин Евгений Юрьевич <eizhboldin@vtb.ru>
2023-08-27 20:50:47 +0300

    init

---
+++ index.js
@@ -0,0 +1,1 @@
+module.exports = require('./src/twinevgeni-env-helper')
---
+++ src/twinevgeni-env-helper.js
@@ -0,0 +1,95 @@
+const appRoot = require('app-root-path');
+const dotenv = require('dotenv');
+const stringHelper = require('twinevgeni-string-helper');
+
+module.exports = function () {
+    /**
+     * load env vars from envPath
+     * @param {string} envPath
+     */
+    function loadEnv(envPath)
+    {
+        try {
+            dotenv.config({path: envPath});
+        } catch (error) {
+
+        }
+    }
+
+    /**
+     * load env vars by envName (in root directory)
+     * @param {string} envName
+     */
+    function loadEnvByName(envName)
+    {
+        try {
+            const envPath = appRoot + '/.' + envName;
+            loadEnv(envPath);
+        } catch (error) {
+
+        }
+    }
+
+    /**
+     * load default env vars (.env file)
+     */
+    function loadDefaultEnv()
+    {
+        loadEnvByName('env');
+    }
+
+    /**
+     * read value or return default
+     * @param value
+     * @param defaultValue
+     * @returns {*|null}
+     */
+    function readEnvValue(value, defaultValue = null)
+    {
+        return !!value ? value : defaultValue;
+    }
+
+    /**
+     * read value or return default
+     * @param value
+     * @param {boolean} defaultValue
+     * @returns {boolean}
+     */
+    function readBoolEnvValue(value, defaultValue = false)
+    {
+        return !!value ? stringHelper.stringToBoolean(value) : defaultValue;
+    }
+
+    /**
+     * read value or return default
+     * @param {string} valueKey
+     * @param defaultValue
+     * @returns {*|null}
+     */
+    function readEnv(valueKey, defaultValue = null)
+    {
+        return readEnvValue(process.env[valueKey], defaultValue);
+    }
+
+    /**
+     * read value or return default
+     * @param {string} valueKey
+     * @param {boolean} defaultValue
+     * @returns {boolean}
+     */
+    function readBoolEnv(valueKey, defaultValue = false)
+    {
+        return readBoolEnvValue(process.env[valueKey], defaultValue);
+    }
+
+    return {
+        loadEnv: loadEnv,
+        loadEnvByName: loadEnvByName,
+        loadDefaultEnv: loadDefaultEnv,
+        readEnv: readEnv,
+        readEnvValue: readEnvValue,
+        readBoolEnvValue: readBoolEnvValue,
+        readEnv: readEnv,
+        readBoolEnv: readBoolEnv
+    }
+}();
